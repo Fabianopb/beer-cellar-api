f29e5965e0f16975f2498d41439b73a6877d97c7
Fabiano Brito <fabianopbrito@gmail.com>
2017-03-29 21:01:10 +0300

    Creates a connection to local mongo database.

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 
+var mongo = require('mongodb');
+
+var MongoServer = mongo.Server;
+var MongoDb = mongo.Db;
+
+var server = new MongoServer('localhost', 28018, {auto_reconnect: true});
+var db = new MongoDb('beerdb', server);
+
+db.open(function(error, database) {
+    if (error) {
+        console.log('Cannot connect to the database!', error);
+    } else {
+        console.log('Successfully connected to the database', database.s.databaseName);
+    }
+});
+
 app.get('/', function(request, response) {
     response.send('Hello World!');
 });
