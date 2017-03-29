5581efb9773df2cff885db694da8761a8c418495
Fabiano Brito <fabianopbrito@gmail.com>
2017-03-29 20:11:44 +0300

    Installs nodemon to automatically restart the server in dev environment.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-    response.send('Hello World');
+    response.send('Hello World!');
 });
 app.get('/beers', function(request, response) {
     response.send('No beers in the database yet!');
