0cee6dd131a136cddd7285269cc6c44070452364
Fabiano Brito <fabianopbrito@gmail.com>
2017-03-30 12:38:26 +0300

    Creates a post request for inserting beers to the database.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 
-// var bodyParser = require('body-parser');
-// var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var bodyParser = require('body-parser').json();
 
 var mongoClient = require('mongodb').MongoClient;
 var db;
@@ -27,6 +26,14 @@ app.get('/beers', function(request, response) {
     db.collection('beers', function(error, collection) {
         collection.find().toArray(function(error, documents) {
             response.json(documents);
+        });
+    });
+});
+app.post('/beers', bodyParser, function(request, response) {
+    var newBeer = request.body;
+    db.collection('beers', function(error, collection) {
+        collection.insertOne(newBeer, function() {
+            response.status(201).json(newBeer);
         });
     });
 });
