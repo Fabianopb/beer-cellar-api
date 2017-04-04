a522c126b889926962836d5913b2895c21f55985
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-04 21:58:46 +0300

    Adds methods to encrypt and validate the password.

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto');
 
 var UserSchema = new Schema({
   email: {
@@ -14,5 +15,15 @@ var UserSchema = new Schema({
   hash: String,
   salt: String
 });
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
 
 module.exports = mongoose.model('User', UserSchema);
