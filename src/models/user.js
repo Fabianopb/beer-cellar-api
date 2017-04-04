2f962761fbcc045fe5caba0c8751694f8de82c13
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-04 22:11:30 +0300

    Adds method for creating a JSON webtoken.

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new Schema({
   email: {
@@ -24,6 +25,18 @@ UserSchema.methods.setPassword = function(password){
 UserSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
   return this.hash === hash;
+};
+
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000, 10),
+  }, process.env.BEER_CELLAR_PASSWORD); // DO NOT KEEP YOUR SECRET IN THE CODE!
 };
 
 module.exports = mongoose.model('User', UserSchema);
