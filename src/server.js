de9d919e29fae45ef9adc6c404e4cad859fe3c35
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-06 22:11:25 +0300

    Includes simple route to test deployment.

--- src/server.js
+++ src/server.js
@@ -19,5 +19,9 @@ app.use(function (err, req, res, next) {
   next();
 });
 
+app.get('/', function(request, response) {
+  response.json('We\'re up and running!');
+});
+
 app.listen(3000);
 console.log('Listening on port 3000...');
