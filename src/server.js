5e275fc95bb0d399d7af44dabe61be75e98ed95b
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-06 22:32:44 +0300

    Listens environment port for Heroku.

--- src/server.js
+++ src/server.js
@@ -23,5 +23,5 @@ app.get('/', function(request, response) {
   response.json('We\'re up and running!');
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('Listening on port 3000...');
