8884da72012c56c34fc71aab3cfdf02265e265c4
Fabiano Brito <fabianopbrito@gmail.com>
2017-04-10 22:00:26 +0300

    Changes Beer model to have a reference of its creator.

--- src/models/beer.js
+++ src/models/beer.js
@@ -2,6 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BeerSchema = new Schema({
+  _creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   name: {
     type: String,
     required: true
